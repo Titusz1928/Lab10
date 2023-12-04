566e613dcc52d0dcc569598482aa42931abbf8a0
Titusz1928 <titusz.boros@student.upt.ro>
2023-12-04 19:11:51 +0200

    p1 done

--- src/main/java/exemplul04/MainApp.java
+++ src/main/java/exemplul04/MainApp.java
@@ -1,2 +1,10 @@
-package exemplul04;public class MainApp {
+package exemplul04;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+class MainApp {
+    public static void main(String[] args) {
+        ApplicationContext context = new ClassPathXmlApplicationContext("beans04.xml");
+        Persoana p = (Persoana) context.getBean("pers");
+        System.out.print(p);
+    }
 }
